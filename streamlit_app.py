e7beb5793fa2c5701bacf5b6c6c2be455d2b8721
lkj000 <lkj@okovanggoai.com>
2024-12-23 06:41:13 +0000

    mycommits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 from rag_system import generate_answer
+
 
 # Initialize chat history and document states
 if "messages" not in st.session_state:
